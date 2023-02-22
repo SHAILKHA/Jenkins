65d1ba961c32c604cc61f25440879a82c7864b3c
shailendra <ss.hn0497@gmail.com>
2023-02-22 07:37:23 +0530

    adding .class file

---
+++ chain1.java
@@ -0,0 +1,8 @@
+class chain1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello my name is shailendra suryavanshi");
+		System.out.println("I have completed my testing cource");
+	}
+
+}
